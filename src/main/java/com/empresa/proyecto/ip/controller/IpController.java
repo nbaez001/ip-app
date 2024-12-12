a21c1e30ccfa0b789c62a4dcaba9a78c9f206aa4
nbaez001 <nbaez001@gmail.com>
2024-12-12 18:45:42 -0500

    Actualizacion de cors

--- src/main/java/com/empresa/proyecto/ip/controller/IpController.java
+++ src/main/java/com/empresa/proyecto/ip/controller/IpController.java
@@ -2,6 +2,7 @@ package com.empresa.proyecto.ip.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 @Slf4j
 @RestController
 @RequestMapping
+@CrossOrigin(origins = "*")
 public class IpController {
 
     @GetMapping
